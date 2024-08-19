5ade06aee33e38fb0762dfa895d37f6eb6615b16
Parth Kathiria <parth.kathiria169@gmail.com>
2024-08-19 08:26:13 -0400

    linked routes

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -8,6 +8,13 @@ export interface Product {
   stockQuantity: number;
 }
 
+export interface NewProduct {
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
 export interface SalesSummary {
   salesSummaryId: string;
   totalValue: number;
@@ -44,15 +51,34 @@ export interface DashboardMetrics {
 }
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),  
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  //All the values like salesSummary, productSummary etc. from dashboardController.ts gets stored in the tagTypes "DashboardMetrics"
-  tagTypes: ["DashboardMetrics"],
+  tagTypes: ["DashboardMetrics", "Products", "Users", "Expenses"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
-      providesTags: ["DashboardMetrics"]
-    })
+      providesTags: ["DashboardMetrics"],
+    }),
+    getProducts: build.query<Product[], string | void>({
+      query: (search) => ({
+        url: "/products",
+        params: search ? { search } : {},
+      }),
+      providesTags: ["Products"],
+    }),
+    createProduct: build.mutation<Product, NewProduct>({
+      query: (newProduct) => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["Products"],
+    }),
   }),
 });
-export const {useGetDashboardMetricsQuery} = api;
+
+export const {
+  useGetDashboardMetricsQuery,
+  useGetProductsQuery,
+  useCreateProductMutation,
+} = api;
