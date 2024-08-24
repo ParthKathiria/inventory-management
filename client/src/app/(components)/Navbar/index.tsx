4a6f385e4954ffff66e5872502a769b6ddab4a0e
Parth Kathiria <parth.kathiria169@gmail.com>
2024-08-24 13:19:07 -0400

    added profile image

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,92 +1,87 @@
 "use client";
 
-import React from 'react'
-import { Bell, Menu, Moon, Settings, Sun } from "lucide-react"
-import Link from 'next/link';
-import { useAppDispatch, useAppSelector } from '@/app/redux';
-import { setIsSidebarCollapsed, setIsDarkMode } from '@/state';
+import React from "react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed, setIsDarkMode } from "@/state";
 
 const Navbar = () => {
-
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
-  
+
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-  }
+  };
 
   const toggleDarkMode = () => {
     dispatch(setIsDarkMode(!isDarkMode));
-  }
+  };
 
   return (
     /* layout for the navigation (search) bar and the icons in the navigation bar*/
     <div className="flex justify-between items-center w-full mb-7">
-        
-        {/* Left side of Navigation bar */}
-        <div className="flex justify-between items-center gap-5"> 
-           
-            {/* ellipsis icon  */}
-            <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
-                    onClick= {toggleSidebar}>
-                <Menu className="w-4 h-4" />
-            </button>
-
-        
+      {/* Left side of Navigation bar */}
+      <div className="flex justify-between items-center gap-5">
+        {/* ellipsis icon  */}
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
 
         {/* for input search*/}
         <div className="relative">
-            <input 
-                type="search" 
-                placeholder="Type to search group or products" 
-                className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" 
-            />
-            {/* Bell icon */}
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <Bell className = "text-gray-500" size={20} />
-            </div>
+          <input
+            type="search"
+            placeholder="Type to search group or products"
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          />
+          {/* Bell icon */}
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <Bell className="text-gray-500" size={20} />
+          </div>
         </div>
-        </div>
-        {/* Right side of navigation bar */}
-        <div className = "flex justify-between items-center gap-5">
-            {/* hiding buttons for smaller screens*/}
-            <div className="hidden md:flex justify-between items-center gap-5">
-                <div>
-                    <button onClick={toggleDarkMode}>
-                        {isDarkMode ? (
-                            <Sun className="cursor-pointer text-gray-500" size={24}/>
-                        ) : (
-                            <Moon className="cursor-pointer text-gray-500" size={24} /> 
-                        )}
-                    </button>
-                </div>
+      </div>
+      {/* Right side of navigation bar */}
+      <div className="flex justify-between items-center gap-5">
+        {/* hiding buttons for smaller screens*/}
+        <div className="hidden md:flex justify-between items-center gap-5">
+          <div>
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
+            </button>
+          </div>
 
-                {/* for bell icon on the right side and the superscript on the bell icon*/}
-                <div className="relative">
-                    <Bell className="cursor-pointer text-gray-500" size={24} />
-                    <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-400 rounded-full">
-                        3 {/* hard coding for now */}
-                    </span>
-                </div>
-                <hr className = "w-0 h-7 border-solid border-l border-gray-300 mx-3" />
-                <div className="flex items-center gap-3 cursor-pointer">
-                    <div className="w-9 h-9"> image </div>
-                    <span className="font-semibold"> Parth</span>
-                </div>
-            </div>
-            {/* Settings icon */}
-            <Link href ="/settings"> 
-                <Settings className="cursor-pointer text-gray-500" size={24} />
-            </Link>
+          {/* for bell icon on the right side and the superscript on the bell icon*/}
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24} />
+            <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-400 rounded-full">
+              3 {/* hard coding for now */}
+            </span>
+          </div>
+          <hr className="w-0 h-7 border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <img src="/user.svg" className="w-9 h-auto" />
+            <span className="font-semibold"> Parth</span>
+          </div>
         </div>
-
+        {/* Settings icon */}
+        <Link href="/settings">
+          <Settings className="cursor-pointer text-gray-500" size={24} />
+        </Link>
+      </div>
     </div>
-
   );
-}
+};
 
 export default Navbar;
